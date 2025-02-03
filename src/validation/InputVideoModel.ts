4c35b63170ef08d2ab090369d86749325732d7d3
VeRg1LiY665 <gtrhr2@gmail.com>
2025-02-04 00:52:56 +0300

    update 1.09

--- src/validation/InputVideoModel.ts
+++ src/validation/InputVideoModel.ts
@@ -3,7 +3,6 @@ import {OutputErrorsType} from "../IO types/OutputErrorsType";
 
 
 const Resolutions = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160']
-//enum Resolutions {P144 = 'P144', P240= 'P240', P360='P360', P480='P480', P720='P720', P1080='P1080', P1440='P1440', P2160='P2160'}
 
 export const inputValidation = (video: InputVideoType) => {
     const errors: OutputErrorsType = { // объект для сбора ошибок
